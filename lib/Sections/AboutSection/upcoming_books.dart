75e84e510cce19af4933f669e0850f7d6af3be9a
ryanemm <59361481+ryanemm@users.noreply.github.com>
2021-10-03 21:57:24 +0200

    book blurb text

--- lib/Sections/AboutSection/upcoming_books.dart
+++ lib/Sections/AboutSection/upcoming_books.dart
@@ -5,6 +5,7 @@ class UpcomingBooksSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(80),
       child: Column(
         children: [
           Row(
@@ -14,9 +15,29 @@ class UpcomingBooksSection extends StatelessWidget {
               Text(
                 "What's coming in 2021",
                 style: GoogleFonts.kaushanScript(
-                    fontSize: 28, fontWeight: FontWeight.w400),
+                    fontSize: 32, fontWeight: FontWeight.w400),
               ),
               Expanded(child: Container(height: 4, color: Colors.pink)),
+            ],
+          ),
+          SizedBox(height: 50),
+          Row(
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Text("Book Title",
+                          style: GoogleFonts.kaushanScript(
+                              fontSize: 28, fontWeight: FontWeight.w300)),
+                      SizedBox(height: 40),
+                      Text(
+                          "Lorem ipsum dolor sit amet, consectetur dgsdf elit, sed fo eiusmod tempor inciduntut ut wingardium leviosa expecto patronum. Thsh skkdf crotore por lae vincitor sdgfj fritres undur parcur magnofactoLorem ipsum dolor sit amet, consectetur dgsdf elit, sed fo eiusmod tempor inciduntut ut wingardium leviosa expecto patronum. Thsh skkdf crotore por lae vincitor sdgfj fritres undur parcur magnofacto, Lorem ipsum dolor sit amet, consectetur dgsdf elit, sed fo eiusmod tempor inciduntut ut wingardium leviosa expecto patronum. Thsh skkdf crotore por lae vincitor sdgfj fritres undur parcur magnofactoLorem ipsum dolor sit amet, consectetur dgsdf elit, sed fo eiusmod tempor inciduntut ut wingardium leviosa expecto patronum. Thsh skkdf crotore por lae vincitor sdgfj fritres undur parcur magnofacto",
+                          style: GoogleFonts.nunito(
+                              fontSize: 20, fontWeight: FontWeight.w300))
+                    ],
+                  )),
+              Expanded(flex: 1, child: Container())
             ],
           )
         ],
